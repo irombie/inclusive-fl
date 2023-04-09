3411f5e80de64eae95e2f2602491af6f1ba19d49
alicerdv <alice.delvecchio@gmail.com>
2023-04-10 00:24:24 +0100

    remove redundant average weights function

--- src/utils.py
+++ src/utils.py
@@ -77,19 +77,6 @@ def get_dataset(args):
 
     return train_dataset, test_dataset, user_groups
 
-
-def average_weights(w):
-    """
-    Returns the average of the weights.
-    """
-    w_avg = copy.deepcopy(w[0])
-    for key in w_avg.keys():
-        for i in range(1, len(w)):
-            w_avg[key] += w[i][key]
-        w_avg[key] = torch.div(w_avg[key], len(w))
-    return w_avg
-
-
 def exp_details(args):
     print('\nExperimental details:')
     print(f'    Model     : {args.model}')

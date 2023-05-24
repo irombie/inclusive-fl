de121250ccaf13de0590495511c2a018f428088b
Sree Harsha Nelaturu <nelaturu.harsha@gmail.com>
2023-05-24 23:38:51 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -176,3 +176,20 @@ def get_dataset_for_metrics(param_dict):
 
     return train_dataset, test_dataset, train_user_groups, test_user_groups
 
+
+def set_seed(seed: int = 42, is_deterministic=False) -> None:
+
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+
+    if is_deterministic:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.use_deterministic_algorithms(True)
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print(f"Random seed set as {seed}")
+

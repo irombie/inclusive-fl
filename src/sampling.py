3e6bc0de637a7aa2307701c61ec262acc5f890f8
alicerdv <alice.delvecchio@gmail.com>
2023-07-20 19:39:49 +0100

    black formatting

--- src/sampling.py
+++ src/sampling.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 from typing import Union, List, Dict
 
+
 def get_iid_partition(dataset, num_users):
     """
     Sample I.I.D. client data from dataset
@@ -15,21 +16,21 @@ def get_iid_partition(dataset, num_users):
     :param num_users:
     :return: dict of image index
     """
-    num_items = int(len(dataset)/num_users)
+    num_items = int(len(dataset) / num_users)
     dict_users, all_idxs = {}, [i for i in range(len(dataset))]
     for i in range(num_users):
-        dict_users[i] = set(np.random.choice(all_idxs, num_items,
-                                             replace=False))
+        dict_users[i] = set(np.random.choice(all_idxs, num_items, replace=False))
         all_idxs = list(set(all_idxs) - dict_users[i])
     return dict_users
 
+
 def paramaterise_noniid_distribution(
-        num_users:int, 
-        num_classes:int, 
-        dataset_labels:Union[torch.Tensor,List], 
-        beta:float, 
-        min_proportion:float=0
-    ):
+    num_users: int,
+    num_classes: int,
+    dataset_labels: Union[torch.Tensor, List],
+    beta: float,
+    min_proportion: float = 0,
+):
     """
     Sample from dirichlet distribution to give non-iid distribution for users.
 
@@ -43,41 +44,53 @@ def paramaterise_noniid_distribution(
         over the users for a specific class
     """
     if isinstance(dataset_labels, list):
-            dataset_labels = torch.tensor(dataset_labels)
+        dataset_labels = torch.tensor(dataset_labels)
     class_weights = np.zeros((num_classes,))
     for class_number in range(num_classes):
-        class_weights[class_number] = (dataset_labels == class_number).sum()/len(dataset_labels)
-    
-    if min_proportion >= 1/num_users:
-        return ValueError(f'min_proportion per user must be less than {1/num_users} for a dataset with {num_users} in it')
+        class_weights[class_number] = (dataset_labels == class_number).sum() / len(
+            dataset_labels
+        )
+
+    if min_proportion >= 1 / num_users:
+        return ValueError(
+            f"min_proportion per user must be less than {1/num_users} for a dataset with {num_users} in it"
+        )
     class_sample_distribution = np.zeros((num_classes, num_users))
     dataset_proportion_per_user = np.zeros(num_users)
     while dataset_proportion_per_user.min() <= min_proportion:
-        class_sample_distribution = np.random.dirichlet(np.repeat(beta, num_users), num_classes)
-        dataset_proportion_per_user = (class_weights@class_sample_distribution)/(class_weights.sum())
+        class_sample_distribution = np.random.dirichlet(
+            np.repeat(beta, num_users), num_classes
+        )
+        dataset_proportion_per_user = (class_weights @ class_sample_distribution) / (
+            class_weights.sum()
+        )
 
     return class_sample_distribution
 
-def get_noniid_partition(dataset_labels:torch.Tensor, distribution:Union[torch.Tensor,List]
-                         ) -> Dict[int, List[int]]:
+
+def get_noniid_partition(
+    dataset_labels: torch.Tensor, distribution: Union[torch.Tensor, List]
+) -> Dict[int, List[int]]:
     """
     Get samples assigned to each user
 
     :param dataset_labels: list or tensor of shape (num_samples,)
     :param distribution: array of shape (num_classes, num_users), where each row is a distribution
         over the users for a specific class
-    
+
     :return: dictionary where each key is a user index, and each item is a list of sample idxs
         for that user
     """
     if isinstance(dataset_labels, list):
-            dataset_labels = torch.tensor(dataset_labels)
+        dataset_labels = torch.tensor(dataset_labels)
     num_classes, num_users = distribution.shape
     sample_idxs = [torch.where(dataset_labels == i)[0] for i in range(num_classes)]
     users_data = defaultdict(list)
     for i in range(num_classes):
         num_class_samples = len(sample_idxs[i])
-        sample_user_idx = np.random.choice(num_users, num_class_samples, p=distribution[i])
+        sample_user_idx = np.random.choice(
+            num_users, num_class_samples, p=distribution[i]
+        )
         for user_idx, sample_idx in zip(sample_user_idx, sample_idxs[i]):
             users_data[user_idx].append(sample_idx.item())
 

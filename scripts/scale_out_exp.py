b3ec11c06cf01107f55a2cd037138e551842f8f4
“krypticmouse” <herumbshandilya123@gmail.com>
2023-09-02 03:28:27 +0530

    Updating Harsha's Script

--- scripts/scale_out_exp.py
+++ scripts/scale_out_exp.py
@@ -4,17 +4,50 @@ import time
 
 import yaml
 from tqdm import tqdm
-
-# Define the parameter combinations
-
+from loguru import logger
 
 def parse_yml(path: str = "scripts/configs.yml"):
     with open(path, "r") as stream:
         try:
+            logger.info("Parsing YAML file.")
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
-            print(exc)
+            logger.error(f"Error parsing YAML file: {exc}")
             return None
+        
+def generate_command_args(combination, gpu, device, timestamp):
+    fl_method = combination[11]
+
+    command_args = {
+        "python3": "src/federated_main.py",
+        "--gpu": gpu,
+        "--device": device,
+        "--model": combination[0],
+        "--lr": combination[1],
+        "--local_ep": combination[2],
+        "--local_bs": combination[3],
+        "--frac": combination[4],
+        "--iid": combination[5],
+        "--alpha": combination[6],
+        "--dataset": combination[7],
+        "--seed": combination[8],
+        "--num_users": combination[9],
+        "--epochs": combination[10],
+        "--fl_method": fl_method,
+        "--wandb_name": f"test_suite_{timestamp}",
+    }
+
+    if fl_method == "FedProx":
+        command_args["--mu"] = combination[12]
+    elif fl_method == "qFedAvg":
+        command_args["--q"] = combination[12]
+        command_args["--eps"] = combination[13]
+    elif fl_method in ["FedAvg", "FedSyn"]:
+        command_args["--sparsification_ratio"] = combination[12]
+        command_args["--sparsification_type"] = combination[13]
+        command_args["--use_fair_sparsification"] = combination[14]
+
+    return command_args
 
 
 def main():
@@ -29,7 +62,7 @@ def main():
         if val_device == "y":
             device = "mps"
         elif val_device != "n":
-            print("i dont understand ur answer, so assigning u cpu ☠️")
+            logger.warning("i dont understand ur answer, so assigning u cpu ☠️")
     else:
         raise Exception("do u have gpu or not? pls specify")
 
@@ -38,77 +71,79 @@ def main():
         raise Exception("Unable to read config file!")
 
     # Generate all parameter combinations
-    parameter_combinations = list(
-        itertools.product(
-            configs["lr"],
-            configs["local_ep"],
-            configs["frac"],
-            configs["iid"],
-            configs["dist_noniid"],
-            configs["mu"],
-            configs["dataset"],
-            configs["reweight_loss_avg"],
-            configs["seed"],
-            configs["num_users"],
-            configs["epochs"],
-            configs["fl_method"],
-        )
-    )
-    timestamp = time.time()
+    parameter_combinations = []
+    for fl_method in configs["fl_method"]:
+        if fl_method == "FedProx":
+            parameter_combinations += list(
+                itertools.product(
+                    configs["model"],
+                    configs["lr"],
+                    configs["local_ep"],
+                    configs["local_bs"],
+                    configs["frac"],
+                    configs["iid"],
+                    configs["alpha"],
+                    configs["dataset"],
+                    configs["seed"],
+                    configs["num_users"],
+                    configs["epochs"],
+                    [fl_method],
+                    configs["mu"],
+                )
+            )
+        
+        elif fl_method == "qFedAvg":
+            parameter_combinations += list(
+                itertools.product(
+                    configs["model"],
+                    configs["lr"],
+                    configs["local_ep"],
+                    configs["local_bs"],
+                    configs["frac"],
+                    configs["iid"],
+                    configs["alpha"],
+                    configs["dataset"],
+                    configs["seed"],
+                    configs["num_users"],
+                    configs["epochs"],
+                    [fl_method],
+                    configs["q"],
+                    configs["eps"],
+                )
+            )
+        
+        else:
+            parameter_combinations += list(
+                itertools.product(
+                    configs["model"],
+                    configs["lr"],
+                    configs["local_ep"],
+                    configs["local_bs"],
+                    configs["frac"],
+                    configs["iid"],
+                    configs["alpha"],
+                    configs["dataset"],
+                    configs["seed"],
+                    configs["num_users"],
+                    configs["epochs"],
+                    [fl_method],
+                    configs["sparsification_ratio"],
+                    configs["sparsification_type"],
+                    configs["use_fair_sparsification"],
+                )
+            )
 
-    # Parse command-line arguments
+    timestamp = time.time()
 
     # Launch experiments
     for i, combination in enumerate(tqdm(parameter_combinations)):
-        (
-            lr,
-            local_ep,
-            frac,
-            iid,
-            dist_noniid,
-            mu,
-            dataset,
-            reweight_test_loss,
-            seed,
-            num_users,
-            epochs,
-            fl_method,
-        ) = combination
+        command_args = generate_command_args(combination, fl_method, gpu, device, timestamp)
+        command = [f"{k}={v}" for k, v in command_args.items()]
 
-        # Apply conditionals
-        if iid == 0:
-            dist_noniid = 1
-        if dist_noniid != 0:
-            iid = 0
-        if fl_method != "FedProx":
-            mu = 0
-        # Generate command-line arguments
-        command = [
-            "python3",
-            "src/federated_main.py",
-            f"--gpu={gpu}",
-            f"--device={device}",
-            "--model=cnn",
-            f"--num_users={num_users}",
-            f"--epochs={epochs}",
-            f"--wandb_name=test_suite_{timestamp}",
-            f"--lr={lr}",
-            f"--local_ep={local_ep}",
-            f"--frac={frac}",
-            f"--iid={iid}",
-            f"--dist_noniid={dist_noniid}",
-            f"--mu={mu}",
-            f"--dataset={dataset}",
-            f"--fl_method={fl_method}",
-            f"--reweight_loss_avg={reweight_test_loss}",
-            f"--seed={seed}",
-        ]
+        logger.info(f'Launching experiment {i+1}/{len(parameter_combinations)}: {" ".join(command)}')
 
-        # Launch the experiment as a subprocess
-        print(
-            f'Launching experiment {i+1}/{len(parameter_combinations)}: {" ".join(command)}'
-        )
         subprocess.run(command)
 
 if __name__ == "__main__":
     main()
+

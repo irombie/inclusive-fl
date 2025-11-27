ed8caba2a343e5d4cd9870ae6814dfd4312fa492
Irem Ergun <iremlergun@gmail.com>
2025-11-27 11:31:20 +0300

    remove redundant files

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -22,7 +22,6 @@ class TestRepositoryStructure:
         required_files = [
             "README.md",
             "pyproject.toml",
-            "environment_droplet.yml",
             "LICENSE",
         ]
 
